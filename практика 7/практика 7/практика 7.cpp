6e8946980f3903acc6c8d51e0a87bc9986a05403
ruzal911 <Ruzal1212@yandex.ru>
2020-10-01 11:34:16 +0300

    Доделал1-3 задание

--- практика 7/практика 7/практика 7.cpp
+++ практика 7/практика 7/практика 7.cpp
@@ -5,10 +5,33 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "RUS");
-     double gradus,a;
+     double gradus,a,X,A,Y;
+    /* cout << "\t\t\t\t_____________Перовое задание_____________ " << endl;
     cout << "Введите значение угла в гардусах: " << endl;
     cin >> a;
     if (a <=360)
         gradus= a / 180;
     cout <<"Угол в радианах равен: "<< gradus <<"pi"<<endl;
+   
+
+    cout << "\t\t\t\t_____________Второе задание_____________ " << endl;
+    cout << "Введите значение угла в радианах: " << endl;
+    cin >> a;
+    if (a <= 360)
+        gradus = a * 180;
+    cout << "Угол в градусах равен: " << gradus <<endl;
+    */
+
+    cout << "\t\t\t\t_____________Третье задание_____________ " << endl;
+    cout << "Введите вес конфет:" << endl;
+    cin >> X;
+    cout << "Введите стоимость конфет:" << endl;
+    cin >> A;
+    A = A / X;
+    cout << "Введите вес конфет:" << endl;
+    cin >> Y;
+    X = Y * A;
+    cout << "Стоимость конфет за 1 кг составляет: " << A << endl;
+    cout << "Стоимость конфет за "<<A<<" кг составляет: " << X << endl;
+
 }
