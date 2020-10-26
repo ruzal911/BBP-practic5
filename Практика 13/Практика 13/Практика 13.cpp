8c22c699d1479c937e8730daee099b3662ef1fd2
ruzal911 <Ruzal1212@yandex.ru>
2020-10-26 15:28:20 +0300

    13

---
+++ Практика 13/Практика 13/Практика 13.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << "\t\t\t\t_________Перове задание_________" << endl;
+	return 0;
+}
---
+++ практика13/практика13/практика13.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, n,R=1;
+	cin >> n;
+	cin >> a;
+	for (int i = 0; i < n; i++)
+	{
+		R *= pow(a, i+1);
+	}
+	cout << R<<endl;
+	return 0;
+
+}
