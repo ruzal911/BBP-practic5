f9469a5faf6e0c523e9589656ddaae2f41f1d169
ruzal911 <Ruzal1212@yandex.ru>
2020-11-05 13:02:37 +0300

    создал 15 практику

---
+++ практика 15/практика 15/практика 15.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int PowerA3(double &A, double &B)
+{
+	B = A*A*A;
+	return B;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	double A, B;
+
+	cout << "\t\t\t\t_________Первое задание_________" << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Введите A:" << endl;
+		cin >> A;
+		PowerA3(A, B);
+		cout << "Результат равен: " << B<<"\n\n";
+	}
+
+
+	return 0;
+}
