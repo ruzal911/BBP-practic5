c9e5c8f365b330709646373b0f57c6f2ae0c4972
ruzal911 <Ruzal1212@yandex.ru>
2020-11-05 15:30:40 +0300

    додлеал 2 задание

--- практика 15/практика 15/практика 15.cpp
+++ практика 15/практика 15/практика 15.cpp
@@ -7,12 +7,22 @@ int PowerA3(double &A, double &B)
 	B = A*A*A;
 	return B;
 }
+int Sign(double &X)
+{
+	if (X < 0)
+		X = -1;
+	if (X = 0)
+		X = 0;
+	if (X >0)
+		X = 1;
+	return X;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	double A, B;
-
+	double A, B,S;
+	/*
 	cout << "\t\t\t\t_________Первое задание_________" << endl;
 	for (int i = 0; i < 5; i++)
 	{
@@ -21,7 +31,17 @@ int main()
 		PowerA3(A, B);
 		cout << "Результат равен: " << B<<"\n\n";
 	}
+	*/
 
+	cout<<"\t\t\t\t_________Второе задание_________" << endl;
+	cout << "Введите A:" << endl;
+	cin >> A;
+	cout << "Введите B:" << endl;
+	cin >> B;
+	A = Sign(A);
+	B=Sign(B);
+	S = A + B;
+	cout << "Значение выражения равно:" <<S<< endl;
 
 	return 0;
 }
